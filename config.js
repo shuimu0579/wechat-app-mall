8c8949cc5d061c0be34d1636c4a3b90ab078727e
吴习章 <wuxz14@vanke.com>
2023-11-17 15:39:57 +0800

    feat: config.js配置

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   version: '14.14.2',
   note: '保存小程序码到手机相册，用户拒绝后给你提示并引导用户打开设置去开启权限', // 这个为版本描述，无需修改
-  subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
-  merchantId: 951, // 商户ID，可在后台工厂设置-->商户信息查看
+  subDomain: 'ea5b3ba2921f10fcb553aedcd46b1082', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
+  merchantId: 59194, // 商户ID，可在后台工厂设置-->商户信息查看
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html
   bindSeller: false, // true 开启三级分销抢客； false 为不开启
   customerServiceType: 'QW' // 客服类型，QW为企业微信，需要在后台系统参数配置企业ID和客服URL，否则为小程序的默认客服
